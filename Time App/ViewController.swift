a13911ebbadee2b2ecffd62e414354c77c5191ec
Kaycee <KayceeT2>
2021-05-06 11:46:14 -0400

    Initial Commit

---
+++ Time App/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    var clocks = [#imageLiteral(resourceName: "12 o' Clock"), #imageLiteral(resourceName: "1 o' Clock"), #imageLiteral(resourceName: "2 o' Clock"), #imageLiteral(resourceName: "3 o' Clock"), #imageLiteral(resourceName: "4 o' Clock"), #imageLiteral(resourceName: "5 o' Clock"), #imageLiteral(resourceName: "6 o' Clock"), #imageLiteral(resourceName: "7 o' Clock"), #imageLiteral(resourceName: "8 o' Clock"), #imageLiteral(resourceName: "9 o' Clock"), #imageLiteral(resourceName: "10 o' Clock"), #imageLiteral(resourceName: "11 o' Clock")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
